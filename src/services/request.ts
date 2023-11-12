b7c556623e804513e8a23d13bd1109e75477cba4
Jones Torres <jonestorresadv@icloud.com>
2023-11-12 18:06:30 -0300

    fix: lang and remove consoles.log

--- src/services/request.ts
+++ src/services/request.ts
@@ -3,7 +3,6 @@ import { api } from "./api"
 export const getItems = async () => {
     try {
         const { data } = await api.get('/items')
-        console.log(data)
         return data
     } catch (error) {
         return error
